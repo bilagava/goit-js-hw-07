3c5b3af9f45ce5882e93af3e470c3f40d72c96af
Yuriy Brovko <ysb.ptc@gmail.com>
2022-05-04 21:41:06 +0300

    js\02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,26 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
+const gallery = galleryItems.map(({original, preview, description})=> 
+    `<div  class="gallery__item">
+        <a class="gallery__link" 
+            href="${original}">
+            <img class="gallery__image"
+            src="${preview}"
+            alt="${description}"/>
+        </a>
+    </div>`).join("");
+
+const galleryContainer = document.querySelector(".gallery");
+
+galleryContainer.insertAdjacentHTML("afterbegin", gallery);
 console.log(galleryItems);
+
+let ligthBox = new SimpleLightbox('.gallery a', {
+    captionData: "alt",
+    captionPosition: "bottom",
+    fadeSpeed: 250,
+});
+
+ligthBox.on("show.simplelightbox", function () {
+    /*something*/
+});
